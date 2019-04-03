71c940381ca45d7de3424543670b969363d393cb
Marius Zander <zandermarius1@gmail.com>
2019-04-03 18:29:38 +0200

    ignoring probe responses

--- bash/dump.sh
+++ bash/dump.sh
@@ -5,6 +5,6 @@ DEVICE=$(printenv 'MONITOR_DEVICE') # Get the device from the enviorment.
 DATE=$(date +"%F")
 
 ./channelhopper.sh 5 &
-tcpdump -i $DEVICE -w $DUMP"wireless_$DATE.cap" -n -e -s 256 type mgt subtype probe-resp or subtype probe-req
+tcpdump -i $DEVICE -w $DUMP"wireless_$DATE.cap" -n -e -s 256 type mgt subtype probe-req
 
 
