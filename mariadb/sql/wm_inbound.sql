41116ca5fa209c34658ac20b9bb3484ac654d22b
Marius Zander <zandermarius1@gmail.com>
2019-05-02 17:35:02 +0200

    Did stuff

--- mariadb/sql/wm_inbound.sql
+++ mariadb/sql/wm_inbound.sql
@@ -1,28 +1,13 @@
 /*
     Datenbank für Rohdaten
 */
+DROP DATABASE IF EXISTS wminbound;
+
 CREATE DATABASE IF NOT EXISTS wminbound;
 USE wminbound;
 /*
     Tabellen    
 */
-CREATE TABLE IF NOT EXISTS sessions (
-    sessionID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    start DATETIME,
-    end DATETIME,
-    setupID INT NOT NULL,
-    PRIMARY KEY(sessionID)
-);
-
-CREATE TABLE IF NOT EXISTS data (
-    dataID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    sigStrength FLOAT NOT NULL,
-    sourceMAC VARCHAR(17),
-    apID INT NOT NULL,
-    time DATETIME,
-    sessionID INT UNSIGNED NOT NULL,
-    PRIMARY KEY(dataID)
-);
 
 CREATE TABLE IF NOT EXISTS location (
    locationID INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -32,37 +17,55 @@ CREATE TABLE IF NOT EXISTS location (
 
 CREATE TABLE IF NOT EXISTS ap (
    apID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-   apName VARCHAR(32) NOT NULL,
-   apPlatform VARCHAR(255),
+   apName                   VARCHAR(32) NOT NULL,
+   apPlatform               VARCHAR(255),
    
-   apNetworkdevIntern VARCHAR(255),
-   apNetworkdevInternMAC VARCHAR(17),
+   apNetworkdevIntern       VARCHAR(255),
+   apNetworkdevInternMAC    VARCHAR(17),
    
-   apNetworkdevExtern VARCHAR(255),
-   apNetworkdevExternMAC VARCHAR(17),
+   apNetworkdevExtern       VARCHAR(255),
+   apNetworkdevExternMAC    VARCHAR(17),
    
-   apComment VARCHAR(255),
+   apComment                VARCHAR(255),
    PRIMARY KEY(apID)
 );
 
-CREATE TABLE IF NOT EXISTS ap_to_setup (
-    ap_to_setupID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    apID INT UNSIGNED NOT NULL,
-    setupID INT UNSIGNED NOT NULL,
-    PRIMARY KEY(ap_to_setupID)
+CREATE TABLE IF NOT EXISTS setup (
+    setupID     INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    locationID  INT UNSIGNED NOT NULL,
+    PRIMARY KEY(setupID),
+    FOREIGN KEY(locationID) REFERENCES location(locationID)
 );
 
-CREATE TABLE IF NOT EXISTS setup (
-    setupID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    locationID INT UNSIGNED NOT NULL,
-    PRIMARY KEY(setupID)
+CREATE TABLE IF NOT EXISTS sessions (
+    sessionID   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    setupID     INT UNSIGNED NOT NULL,
+    start       DATETIME,
+    end         DATETIME,
+    PRIMARY KEY(sessionID),
+    FOREIGN KEY(setupID) REFERENCES setup(setupID)
+);
+
+CREATE TABLE IF NOT EXISTS data (
+    dataID      INT UNSIGNED    NOT NULL AUTO_INCREMENT,
+    apID        INT UNSIGNED    NOT NULL,
+    sessionID   INT UNSIGNED    NOT NULL,
+    sigStrength FLOAT           NOT NULL,
+    sourceMAC   VARCHAR(17),
+    time        DATETIME,
+    PRIMARY KEY(dataID),
+    FOREIGN KEY(sessionID) REFERENCES sessions(sessionID),
+    FOREIGN KEY(apID) REFERENCES ap(apID)
 );
 
 CREATE TABLE IF NOT EXISTS distance (
-   distanceID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-   fromApID INT NOT NULL,
-   toApID INT NOT NULL,
-   distance FLOAT NOT NULL,
-   sessionID INT NOT NULL,
-   PRIMARY KEY(distanceID)
+   distanceID   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   fromApID     INT UNSIGNED NOT NULL,
+   toApID       INT UNSIGNED NOT NULL,
+   sessionID    INT UNSIGNED NOT NULL,
+   distance     FLOAT NOT NULL,
+   PRIMARY KEY(distanceID),
+   FOREIGN KEY(fromApID) REFERENCES ap(apID),
+   FOREIGN KEY(toApID) REFERENCES ap(apID),
+   FOREIGN KEY(sessionID) REFERENCES sessions(sessionID)
 );
