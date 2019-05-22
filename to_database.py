ab4048be230b5d3d0d7f458c5371af497c28c83c
Marius Zander <zandermarius1@gmail.com>
2019-05-22 11:34:43 +0200

    put sql stuff in seperate file

--- to_database.py
+++ to_database.py
@@ -1,41 +1,19 @@
 #!/usr/bin/python2
-
-import mysql.connector as mariadb
 import sys
 import time
 import json
+import os
 
 with open("./conf.json", 'r') as f:
     config = json.load(f)
 
 AP_NUMBER = config["ap"]["number"]
 
-SERVER_IP = config["db"]["ip"]
-SERVER_PORT = config["db"]["port"]
-SERVER_USER = config["db"]["user"]
-SERVER_PASS = config["db"]["pass"]
-SERVER_DB = config["db"]["db_name"]
-
 CURRENT_SESSION = "0"
 LOCATION = "LOCATION_A"
 
 
-def connect():
-    print("Trying to connect to " + SERVER_IP + ":" +  SERVER_PORT + " as " + SERVER_PASS)
-    retry = True
-    while retry: 
-        try:    
-            con = mariadb.connect(host=SERVER_IP,port=SERVER_PORT,user=SERVER_USER, password=SERVER_PASS, database=SERVER_DB)
-            retry = False
-            print("Connected!\r\n")
-        except:
-            print("Retrying to connect to " + SERVER_IP + " as " + SERVER_PASS + " in 5 seconds...")
-            retry = True
-            time.sleep(5)
-    return con
 
-
-connection = connect()
 
 def sendToDB(data):
     splt = data.split()
@@ -49,12 +27,10 @@ def sendToDB(data):
     print("Strength: " + strength)
     print("Source: " + source)
     
+    query = "INSERT INTO data (apID,sessionID,sigStrength,strength,sourceMAC,time) VALUES (" + AP_NUMBER + "," +CURRENT_SESSION + "," + strength + "," + source,_time + ")"
+    os.system("./exec_sql_query.py \"" + query + "\"")
 
-    try:
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO data (apID,sessionID,sigStrength,strength,sourceMAC,time) VALUES (%s,%s,%s,%s,%s)", (AP_NUMBER, CURRENT_SESSION, strength, source, _time))
-    except:
-        print("Failed to insert into DB!\r\n")
+
 
                 
         
